d1164320a2f47f28b92a92147a19ca8259546edc
RamnikDhoot <99842947+RamnikDhoot@users.noreply.github.com>
2023-11-22 21:27:21 +0000

    Create Items.jsx

---
+++ src/components/Items.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+function main(){
+    return(
+<section id="clients" class="clients section-bg">
+    <div class="container">
+      <div class="row" data-aos="zoom-in">
+        <div
+          class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center"
+        >
+          <img
+            src="images/placeholder.gif"
+            class="img-fluid"
+            alt=""
+          />
+        </div>
+
+        <div
+          class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center"
+        >
+          <img
+          src="images/placeholder.gif"
+          class="img-fluid"
+            alt=""
+          />
+        </div>
+
+        <div
+          class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center"
+        >
+          <img
+          src="images/placeholder.gif"
+          class="img-fluid"
+            alt="---------"
+          />
+        </div>
+
+        <div
+          class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center"
+        >
+          <img
+          src="images/placeholder.gif"
+          class="img-fluid"
+            alt="----------------"
+          />
+        </div>
+
+        <div
+          class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center"
+        >
+          <img
+          src="images/placeholder.gif"
+          class="img-fluid"
+            alt="------------------"
+          />
+        </div>
+
+        <div
+          class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center"
+        >
+          <img
+          src="images/placeholder.gif"
+          class="img-fluid"
+            alt="------------"
+          />
+        </div>
+      </div>
+    </div>
+  </section>);
+}
+
+export default Navbar;
